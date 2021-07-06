494d79248cf8c8cde589e979db0ca95dfbbca901
Hicka <hello@hicka.dev>
2021-07-06 14:27:45 +0200

    docs(jsdoc): make JSDoc comments always multiline

--- src/types/ArticleCardProps.ts
+++ src/types/ArticleCardProps.ts
@@ -3,15 +3,25 @@
  * Corresponds to the data of a blog article.
  */
 interface ArticleCardProps {
-	/** Writing date of the article */
+	/** 
+	 * Writing date of the article 
+	 */
 	date: Date,
-	/** Short extract of the article. */
+	/** 
+	 * Short extract of the article. 
+	 */
 	description: string,
-	/** Unique id of the article. Can be understandable words. */
+	/** 
+	 * Unique id of the article. Can be understandable words. 
+	 */
 	id: string
-	/** Name of the article's image. */
+	/** 
+	 * Name of the article's image. 
+	 */
 	img: string,
-	/** Title of the article. */
+	/** 
+	 * Title of the article. 
+	 */
 	title: string,
 }
 export default ArticleCardProps;
--- src/types/WarnProps.ts
+++ src/types/WarnProps.ts
@@ -2,9 +2,13 @@
  * Props to pass to a Warn component.
  */
 interface WarnProps {
-	/** Title of the Warn, displayed in bold. */
+	/** 
+	 * Title of the Warn, displayed in bold. 
+	 */
 	title?: string;
-	/** The message of the Warn. */
+	/** 
+	 * The message of the Warn. 
+	 */
 	message: string;
 	/** Whether to make the Warn closable by the user.
 	 * Making `persistent` true will not let the user close the Warn.
