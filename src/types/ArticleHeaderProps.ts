17664f24e57a25f6439f3a7cc95474eb64aa77a6
Hicka <hello@hicka.dev>
2021-07-06 14:27:19 +0200

    docs(jsdoc): add doc for ArticleHeaderProps

--- src/types/ArticleHeaderProps.ts
+++ src/types/ArticleHeaderProps.ts
@@ -1,6 +1,19 @@
+/**
+ * Props for an ArticleHeader component.
+ */
 interface ArticleHeaderProps {
+	/**
+	 * The name of the background image for the component.
+	 * Retrieved in /public/thumbnails.
+	 */
 	banner: string;
+	/**
+	 * The date the article was published.
+	 */
 	date: Date;
+	/**
+	 * The title of the Article
+	 */
 	title: string;
 }
 
