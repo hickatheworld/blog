501f811d00e5132641ef29b8174338c87ee46f9b
Hicka <hello@hicka.dev>
2021-07-10 15:14:04 +0200

    style(types): remove useless spaces in JSDoc

--- src/types/HomeHeaderProps.ts
+++ src/types/HomeHeaderProps.ts
@@ -4,20 +4,20 @@ import WarnProps from './WarnProps';
  * Props to pass to a HomeHeader component.
  */
 interface HomeHeaderProps {
-	/** 
+	/**
 	 * Descriptions about me/the blog to display alternatively. 
 	 */
 	descs: string[];
-	/** 
+	/**
 	 * The title of the blog.
 	 * @default hickatheblog
 	 */
 	title: string;
-	/** 
+	/**
 	 * Any message to display below the descriptions. 
 	 */
 	motd?: string;
-	/** 
+	/**
 	 * Optional important message to display below the Header. 
 	 */
 	warn?: WarnProps;
