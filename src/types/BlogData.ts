d2653b7456f38e34ec8c5d2f28b7d6c7c3f72a52
Hicka <hello@hicka.dev>
2021-07-18 11:13:47 +0200

    cleanup(types): remove articles from BlogData
    This value is not needed, the articles list will be fetched from GitHub API

--- src/types/BlogData.ts
+++ src/types/BlogData.ts
@@ -1,14 +1,9 @@
-import ArticleCardProps from './ArticleCardProps';
 import WarnProps from './WarnProps';
 
 /**
  * Index data of the blog for Home.
  */
 interface BlogData {
-	/**
-	 * An array of ArticleCardProps, ordered by Date (most recent first).
-	 */
-	articles: ArticleCardProps[];
 	/**
 	 * An optional motd.
 	 */
