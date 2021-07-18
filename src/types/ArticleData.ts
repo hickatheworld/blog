39c8f9316d85ac9cdb5c0279f1af068e7c8ba6e7
Hicka <hello@hicka.dev>
2021-07-18 12:04:10 +0200

    refactor(types): simplified ArticleData
    Using interfaces extension

--- src/types/ArticleData.ts
+++ src/types/ArticleData.ts
@@ -1,23 +1,13 @@
+import ArticleCardProps from './ArticleCardProps';
+
 /**
  * Data of a blog Article
  */
-interface ArticleData {
+interface ArticleData extends ArticleCardProps {
 	/**
 	 * The Markdown content of the Article.
 	 */
 	content: string;
-	/**
-	 * The time the Article was written at.
-	 */
-	createdAt: Date;
-	/**
-	 * The name of the Article's thumbnail.
-	 */
-	thumbnail: string;
-	/**
-	 * The title of the Article.
-	 */
-	title: string;
 }
 
 export default ArticleData;
