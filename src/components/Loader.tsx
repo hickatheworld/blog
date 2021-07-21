adb1d79a3acbb85de02387e2ac5dd85c85a9e22e
Hicka <hello@hicka.dev>
2021-07-21 11:51:03 +0200

    feat(components): add Loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+import '../styles/Loader.sass';
+
+/**
+ * Visual indicator of loading content.
+ */
+function Loader() {
+	return (
+		<div className='loader'></div>
+	);
+}
+
+export default Loader;
