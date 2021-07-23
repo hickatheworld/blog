2893cd2175a0de272a85c2d2687c3aec5898e17a
Hicka <hello@hicka.dev>
2021-07-23 15:19:03 +0200

    feat(components): make Loader resizable

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,10 +2,11 @@ import '../styles/Loader.sass';
 
 /**
  * Visual indicator of loading content.
+ * @param size.number The size in pixels of the loader
  */
-function Loader() {
+function Loader(props: { size?: number }) {
 	return (
-		<div className='loader'></div>
+		<div className='loader' style={{ 'width': `${props.size || 25}px`, 'height': `${props.size || 25}px` }}></div>
 	);
 }
 
