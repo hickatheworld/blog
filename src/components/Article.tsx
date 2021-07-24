a0a8661df2f49695e5639780acaecd9835be6a54
Hicka <hello@hicka.dev>
2021-07-24 10:54:44 +0200

    feat(components): add link to home in Article

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown';
+import { Link } from 'react-router-dom';
 import '../styles/Article.sass';
 import ArticleData from '../types/ArticleData';
 import ArticleHeader from './ArticleHeader';
@@ -19,6 +20,10 @@ function Article(props: ArticleData) {
 					{props.content}
 				</ReactMarkdown>
 			</div>
+			<Link to='/' className='article-home-btn'>
+				<span className='material-icons'>home</span>
+				Home
+			</Link>
 		</div>
 	);
 }
